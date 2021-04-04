7d5f09b271fdabf12d5cc4b5ada74ea7ec18dcb9
Moh. Bahrul Ulum <48970016+ullum@users.noreply.github.com>
2021-04-04 21:45:49 +0700

    task-admin-03
    login success
    logout succes manual sytax
    
    #4 unpas

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -11,10 +11,57 @@ class Auth extends CI_Controller
 
     public function index()
     {
-        $data['title'] = 'Login Page';
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/login');
-        $this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Login Page';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('templates/auth_footer');
+        } else {
+            // validasi success
+            $this->_login();
+        }
+    }
+
+    private function _login()
+    {
+
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+        // var_dump($user);
+        // die;
+
+        if ($user) {
+            // usernya ada
+            if ($user) {
+                // jika sudah active
+                if ($user['is_active'] == 1) {
+                    // cek password
+                    if (password_verify($password, $user['password'])) {
+                        $data = [
+                            'email' => $user['email'],
+                            'role_id' => $user['role_id']
+                        ];
+                        $this->session->set_userdata($data);
+                        // ke halaman user
+                        redirect('user');
+                    } else {
+                        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> Password salah </div>');
+                        redirect('auth');
+                    }
+                } else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Email Belum Active</div>');
+                    redirect('auth');
+                }
+            }
+        } else {
+            // jika gagal/belum terdatar
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Email Belum Mendaftar</div>');
+            redirect('auth');
+        }
     }
 
     public function registration()
@@ -41,7 +88,7 @@ class Auth extends CI_Controller
                 'name' => htmlspecialchars($this->input->post('name', true)),
                 'email' => htmlspecialchars($this->input->post('email', true)),
                 'image' => 'default.jpg',
-                'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
                 'role_id' => 2,
                 'is_active' => 1,
                 'date_created' => time()
@@ -51,5 +98,15 @@ class Auth extends CI_Controller
             $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> Berhasil Mendaftar</div>');
             redirect('auth');
         }
+    }
+
+
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> Berhasil Logout</div>');
+        redirect('auth');
     }
 }
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -20,12 +20,14 @@
 
 
 
-                                <form class="user">
+                                <form class="user" method="post" action="<?= base_url('auth'); ?>">
                                     <div class="form-group">
-                                        <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address...">
+                                        <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address..." value="<?= set_value('email'); ?>">
+                                        <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
                                     <div class="form-group">
                                         <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+                                        <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
 
                                     <button type="submit" class="btn btn-primary btn-user btn-block">
