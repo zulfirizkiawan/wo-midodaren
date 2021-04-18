7259a3a469f308cd713c19d01929093abfee9d6a
Moh. Bahrul Ulum <48970016+ullum@users.noreply.github.com>
2021-04-18 09:18:59 +0700

    Update sidebar_ad.php
    backticks  (   `    )
    &
    petik satu (    '   )
    
    beda

--- application/views/templates/sidebar_ad.php
+++ application/views/templates/sidebar_ad.php
@@ -16,11 +16,11 @@
     <!-- Query Menu -->
     <?php
     $role_id = $this->session->userdata('role_id');
-        $queryMenu = "SELECT 'user_menu'.'id', 'menu'
-                          FROM 'user_menu' JOIN 'user_access_menu'
-                            ON 'user_menu'.'id' = 'user_access_menu'.'menu_id'
-                         WHERE 'user_access_menu'.'role_id' = $role_id
-                      ORDER BY 'user_access_menu'.'menu_id' ASC
+        $queryMenu = "SELECT `user_menu`.`id`, `menu`
+                        FROM `user_menu` JOIN `user_access_menu`
+                        ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+                    WHERE `user_access_menu`.`role_id` = $role_id
+                    ORDER BY `user_access_menu`.`menu_id` ASC
                         ";
 $menu = $this->db->query($queryMenu)->result_array();
 var_dump($menu);
