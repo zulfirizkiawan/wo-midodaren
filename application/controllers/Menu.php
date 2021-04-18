7dbd86c38aad2fe06c48a79b6fe5c44d8b25c7b0
Moh. Bahrul Ulum <48970016+ullum@users.noreply.github.com>
2021-04-18 15:50:02 +0700

    #7 submenu - no fix
    capek

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -28,9 +28,31 @@ class Menu extends CI_Controller
    }
 
 
-   // public function addMenu()
-   // {
+   public function submenu()
+   {
 
+      $data['title'] = 'Submenu Management';
+      $data['user'] = $this->db->get_where('user', ['email' =>
+      $this->session->userdata('email')])->row_array();
 
-   // }
+      //Alias
+      $this->load->model('Menu_model', 'menu');
+      $data['subMenu'] = $this->menu->getSubMenu();
+      // $data['subMenu'] = $this->db->get('user_sub_menu')->result_array();
+      $data['menu'] = $this->db->get('user_menu')->result_array();
+
+      $this->form_validation->set_rules('title', 'Title', 'required');
+      $this->form_validation->set_rules('menu', 'Menu', 'required');
+      $this->form_validation->set_rules('url', 'URL', 'required');
+      $this->form_validation->set_rules('icon', 'Icon', 'required');
+
+      if($this->form_validation->run() == false){
+         $this->load->view('templates/header_ad', $data);
+         $this->load->view('templates/sidebar_ad', $data);
+         $this->load->view('templates/topbar_ad', $data);
+         $this->load->view('menu/submenu', $data);
+         $this->load->view('templates/footer_ad');
+
+      }
+   }
 }
