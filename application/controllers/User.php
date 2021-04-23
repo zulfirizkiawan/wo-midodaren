7965a3900f0ffe49ae144f32446863f2d1f7e239
Moh. Bahrul Ulum <48970016+ullum@users.noreply.github.com>
2021-04-23 12:22:43 +0700

    a

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -22,4 +22,29 @@ class User extends CI_Controller
         $this->load->view('user/index', $data);
         $this->load->view('templates/footer_ad',);
     }
+
+    public function paket()
+   {
+      $data['title'] = 'paket';
+      $data['user'] = $this->db->get_where('user', ['email' =>
+      $this->session->userdata('email')])->row_array();
+      // echo 'selamat datang ' . $data['user']['name'];
+
+      $data['userp'] = $this->db->get('paket')->result_array();
+
+      $this->form_validation->set_rules('paket', 'User', 'required');
+
+      if ($this->form_validation->run() == false) {
+         $this->load->view('templates/header_ad', $data);
+         $this->load->view('templates/sidebar_ad', $data);
+         $this->load->view('templates/topbar_ad', $data);
+         $this->load->view('user/paket', $data);
+         $this->load->view('templates/footer_ad',);
+      } else {
+         $this->db->insert('paket', ['userp' => $this->input->post('paket')]);
+         $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> New Menu Added! </div>');
+         redirect('user/paket');
+      }
+   }
+
 }
---
+++ application/views/user/paket.php
@@ -0,0 +1,94 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+
+
+    <div class="row">
+        <div class="col-lg-6">
+            <?= form_error('user', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
+
+            <?= $this->session->flashdata('message') ?>
+
+            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newMenuModal">Add New Menu</a>
+
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">jenis_paket</th>
+                        <th scope="col">nama_paket</th>
+                        <th scope="col">harga</th>
+                        <th scope="col">rias_busana</th>
+                        <th scope="col">dekorasi_pelaminan</th>
+                        <th scope="col">dokumentasi</th>
+                        <th scope="col">dekorasi_tenda</th>
+                        <th scope="col">support_acara</th>
+                        <th scope="col">minimal_dp</th>
+                        
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1; ?>
+                    <?php foreach ($userp as $m) : ?>
+                        <tr>
+                            <th scope="row"><?= $i ?></th>
+                            
+                            <td><?= $m['jenis_paket']; ?></td>
+                            <td><?= $m['nama_paket']; ?></td>
+                            <td><?= $m['harga']; ?></td>
+                            <td><?= $m['rias_busana']; ?></td>
+                            <td><?= $m['dekorasi_pelaminan']; ?></td>
+                            <td><?= $m['dokumentasi']; ?></td>
+                            <td><?= $m['dekorasi_tenda']; ?></td>
+                            <td><?= $m['support_acara']; ?></td>
+                            <td><?= $m['minimal_dp']; ?></td>
+                            
+                            <td>
+                                <a href="" class="badge badge-pill badge-success">edit</a>
+                                <a href="" class="badge badge-pill badge-danger">delete</a>
+                            </td>
+                        </tr>
+                        <?php $i++; ?>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+<!-- Modal -->
+
+
+<!-- Modal -->
+<div class="modal fade" id="newMenuModal" tabindex="-1" aria-labelledby="newMenuModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="newMenuModalLabel">Add New Menu</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="<?= base_url('user/paket');  ?>" method="post">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="paket" name="paket" placeholder="Menu name">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Add</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
