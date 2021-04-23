a42c723292ac89b228cc4abfbdad663a1783929d
Moh. Bahrul Ulum <48970016+ullum@users.noreply.github.com>
2021-04-23 07:12:32 +0700

    #8

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -112,4 +112,9 @@ class Auth extends CI_Controller
         $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> Berhasil Logout</div>');
         redirect('auth');
     }
+
+
+    public function blocked(){
+        $this->load->view('auth/blocked');
+    }
 }
