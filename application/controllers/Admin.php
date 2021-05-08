099e61a6baadf6f94574adbc08a9296f1e35df73
Moh. Bahrul Ulum <48970016+ullum@users.noreply.github.com>
2021-05-08 09:03:34 +0700

    #9 - 1/2
    bentar

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -23,4 +23,21 @@ class Admin extends CI_Controller
         $this->load->view('admin/index', $data);
         $this->load->view('templates/footer_ad',);
     }
+
+    public function role()
+    {
+        $data['title'] = 'Role';
+        $data['user'] = $this->db->get_where('user', ['email' =>
+        $this->session->userdata('email')])->row_array();
+        // echo 'selamat datang ' . $data['user']['name'];
+
+
+        $data['role'] = $this->db->get('user_role')->result_array();
+
+        $this->load->view('templates/header_ad', $data);
+        $this->load->view('templates/sidebar_ad', $data);
+        $this->load->view('templates/topbar_ad', $data);
+        $this->load->view('admin/role', $data);
+        $this->load->view('templates/footer_ad',);
+    }
 }
