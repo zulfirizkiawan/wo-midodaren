473db76a37b8af0053c11e32b18d8af4ad30d2b9
Moh. Bahrul Ulum <48970016+ullum@users.noreply.github.com>
2021-05-18 12:38:02 +0700

    update

--- application/views/templates/footer_ad.php
+++ application/views/templates/footer_ad.php
@@ -76,13 +76,13 @@
 </script>
 
 <!-- Placed at the end of the document so the pages load faster -->
-<!-- <script src="<?php echo base_url()?>assets/jquery/jquery-3.2.1.min.js"></script> -->
+
     <script src="<?php echo base_url()?>assets/js/arf.js"></script>
     <script src="<?php echo base_url()?>assets/js/prs.js"></script>
     <script src="<?php echo base_url()?>assets/js/validation.js"></script>
     <script src="<?php echo base_url()?>assets/jquery/jquery-ui.js"></script>
     <script src="<?php echo base_url()?>assets/jquery/jquery.validate.min.js"></script>
-    <script>window.jQuery || document.write('<script src="<?php echo base_url()?>assets/vendor/jquery.min.js"><\/script>')</script>
+
     <script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap.min.js"></script>
 
 </body>
