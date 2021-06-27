cee2023099c971326682ab9c4c97aa3ae83a4d94
Moh. Bahrul Ulum <48970016+ullum@users.noreply.github.com>
2021-06-27 09:05:40 +0700

    Update list_produk.php

--- application/views/pemesanan/list_produk.php
+++ application/views/pemesanan/list_produk.php
@@ -36,18 +36,7 @@
     ?>
   </div>
 
-  <!-- KATEGORI -->
-  <div class="list-group">
-    <a class="list-group-item"><strong>KATEGORI</strong></a>
-    <a href="<?php echo base_url() ?>pemesanan/index/" class="list-group-item">Semua</a>
-    <?php
-    foreach ($kategori as $row) {
-    ?>
-      <a href="<?php echo base_url() ?>pemesanan/index/<?php echo $row['id']; ?>" class="list-group-item"><?php echo $row['nama_kategori']; ?></a>
-    <?php
-    }
-    ?>
-  </div><br>
+  
 
 </div>
 <br>
